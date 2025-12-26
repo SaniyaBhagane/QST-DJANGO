cbd95db760fd5fcff2b86b244dc7cbaf4291da57
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2025-12-26 15:02:09 +0530

    modelss

--- modelsProject/testModels/models.py
+++ modelsProject/testModels/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 
 # Create your models here.
-# saniya123qst
+# pass for superuser = saniya123qst
 class Student(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     enrollment_date = models.DateField()
     enrollment_number = models.CharField(max_length=50, null=True, blank=True)
     major = models.CharField(max_length=50, null=True, blank=True)
+    
     
     # def __str__(self):
     #     return f"{self.first_name} {self.last_name}"

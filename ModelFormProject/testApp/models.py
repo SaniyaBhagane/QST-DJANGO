a78c004646ca93201af0ff54b2aed6b4766efa3f
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2025-12-27 11:43:45 +0530

    forms&views

--- ModelFormProject/testApp/models.py
+++ ModelFormProject/testApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    enrollment_date = models.DateField()
+    enrollment_number = models.CharField(max_length=50, null=True, blank=True)
+    major = models.CharField(max_length=50, null=True, blank=True)
+    
+    
--- ModelFormProject/testApp/views.py
+++ ModelFormProject/testApp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from testApp.forms import StudentForm
 
 # Create your views here.
+def from_view(request):
+    form = StudentForm()
+    return render(request, 'testApp/StudentForm.html', {'form': form})

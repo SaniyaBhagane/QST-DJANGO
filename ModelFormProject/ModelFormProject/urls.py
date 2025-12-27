f9b1e55aad9fc4beeff568bd4b20e40fe7db966a
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2025-12-27 12:37:50 +0530

    styled Pages

--- ModelFormProject/ModelFormProject/urls.py
+++ ModelFormProject/ModelFormProject/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from testApp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.from_view, name='student-form'),
     path('list/', views.student_list, name='student-list'),
     path('detail/<int:pk>/', views.student_detail, name='student-detail'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

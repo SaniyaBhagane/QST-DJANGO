9ba67e65f50239132cd16bb5890f8a43ace72759
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2025-12-28 18:44:13 +0530

    dashboardhtml&view

--- ModelFormProject2/testApp/views.py
+++ ModelFormProject2/testApp/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_list_or_404
+from .forms import UserProfileForm
+from .models import UserProfile
+
 
 # Create your views here.
+def dashboard(request):
+    profiles = UserProfile.objects.all()
+    return render(request, 'dashboard.html', {'profiles': profiles})

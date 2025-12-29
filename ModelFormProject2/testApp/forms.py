8a0e6669bb420802bbcee183972dbffa61e7a6c1
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2025-12-29 22:52:46 +0530

    updated

--- ModelFormProject2/testApp/forms.py
+++ ModelFormProject2/testApp/forms.py
@@ -2,7 +2,20 @@ from django import forms
 from .models import UserProfile
 
 class UserProfileForm(forms.ModelForm):
+
+    skills = forms.MultipleChoiceField(
+        choices=UserProfile.SKILL_CHOICES,
+        widget=forms.CheckboxSelectMultiple
+    )
+
     class Meta:
         model = UserProfile
-        fields = '__all__' 
-    
+        fields = "__all__"
+        widgets = {
+            'gender': forms.RadioSelect,
+            'dob': forms.DateInput(attrs={'type': 'date'}),
+        }
+
+    def clean_skills(self):
+        # Convert list → string for DB storage
+        return ",".join(self.cleaned_data['skills'])
--- ModelFormProject2/testApp/models.py
+++ ModelFormProject2/testApp/models.py
@@ -4,10 +4,17 @@ from django.db import models
 # saniya1234
 
 class UserProfile(models.Model):
+    
+    GENDER_CHOICES = (
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+        ('Other', 'Other'),
+    )
+
     username = models.CharField(max_length=150, unique=True)
     email = models.EmailField(unique=True)
     about = models.TextField(blank=True, null=True)
-    gender = models.CharField(max_length=10)
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
     profile_picture = models.ImageField(upload_to='profile_pics/')
     skills = models.CharField(max_length=255)
     dob = models.DateField()

c018e0d0cb60b21b6d2f4bfd7059114451dcc924
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2026-01-10 12:26:42 +0530

    setup done

--- ToDoApp/TODO/admin.py
+++ ToDoApp/TODO/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import todo
 
-# Register your models here.
+# Register your models here.\
+
+class todolist(admin.ModelAdmin):
+    list_display = ('taskName', 'dateTime')
+
+admin.site.register(todo)
--- ToDoApp/TODO/models.py
+++ ToDoApp/TODO/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+# todoapp1234
 # Create your models here.
+class todo(models.Model):
+        taskName = models.CharField(max_length= 100)
+        dateTime = models.DateTimeField(auto_now_add= True)
+        
+    
+    

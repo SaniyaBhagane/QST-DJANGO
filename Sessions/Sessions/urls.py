4dc15b216eb8660d5ade42a8cd9f89872e8cb181
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2026-01-31 11:51:09 +0530

    //

--- Sessions/Sessions/urls.py
+++ Sessions/Sessions/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from testapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('pagecount/', views.page_count, name='page_count'),
 ]

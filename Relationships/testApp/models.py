e8ed5abb491ce4ee69551bd11f252a4fb78376c0
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2026-02-07 12:51:07 +0530

    migrations

--- Relationships/testApp/models.py
+++ Relationships/testApp/models.py
@@ -22,6 +22,3 @@ class Father(models.Model):
 class Children(models.Model):
     father = models.ForeignKey(Father, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    
-    def __str__(self):
-        return f'{self.name}'

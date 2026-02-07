dea3a36ede4691e96756bfce28717d1f688ded10
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2026-02-07 12:49:28 +0530

    bew admin registration

--- Relationships/testApp/admin.py
+++ Relationships/testApp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Person, AadharCard
+from .models import Person, AadharCard, Father, Children
 
 # Register your models here.
 class PersonAdmin(admin.ModelAdmin):
@@ -10,3 +10,12 @@ class AadharCardAdmin(admin.ModelAdmin):
     
 admin.site.register(Person, PersonAdmin)
 admin.site.register(AadharCard, AadharCardAdmin)
+
+class FatherAdmin(admin.ModelAdmin):
+    list_display = ('name', 'age')
+    
+class ChildrenAdmin(admin.ModelAdmin):
+    list_display = ('father', 'name')
+    
+admin.site.register(Father, FatherAdmin)
+admin.site.register(Children, ChildrenAdmin)

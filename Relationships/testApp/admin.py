3ec375625c8755b59d3712c12795607f68952c8c
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2026-02-07 12:32:24 +0530

    onetoone

--- Relationships/testApp/admin.py
+++ Relationships/testApp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Person, AadharCard
 
 # Register your models here.
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('name', 'age')
+    
+class AadharCardAdmin(admin.ModelAdmin):
+    list_display = ('person', 'number')
+    
+admin.site.register(Person, PersonAdmin)
+admin.site.register(AadharCard, AadharCardAdmin)

673a910c5a07ab5b3f630e1f2d7a4951badf1c0d
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2026-02-07 12:46:58 +0530

    new model

--- Relationships/testApp/models.py
+++ Relationships/testApp/models.py
@@ -11,3 +11,17 @@ class Person(models.Model):
 class AadharCard(models.Model):
     person = models.OneToOneField(Person, on_delete=models.CASCADE)
     number = models.CharField(max_length=12)
+    
+class Father(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    
+    def __str__(self):
+        return f'{self.name}'
+
+class Children(models.Model):
+    father = models.ForeignKey(Father, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f'{self.name}'
